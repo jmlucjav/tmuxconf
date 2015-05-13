6e2829d53bf1d3366d4acb4411839519db24f4d2
jmlucjav <jmlucjav@gmail.com>
2015-05-13 14:52:41 +0200

    sh updates

--- updateconf.sh
+++ updateconf.sh
@@ -8,6 +8,7 @@ wget -q https://raw.githubusercontent.com/jmlucjav/tmuxconf/master/.tmux.conf -O
 wget -q https://raw.githubusercontent.com/jmlucjav/tmuxconf/master/.tmux.solr.session -O .tmux.solr.conf
 wget -q https://raw.githubusercontent.com/jmlucjav/tmuxconf/master/.bashrc -O .bashrc
 wget -q https://raw.githubusercontent.com/jmlucjav/tmuxconf/master/.shared_aliases -O .shared_aliases
+wget -q https://raw.githubusercontent.com/jmlucjav/tmuxconf/master/dircolors.ansi-light -O dircolors.ansi-light
 
 #start session with tmux
 tmux kill-server
