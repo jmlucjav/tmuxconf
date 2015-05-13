64befba140658021f0e3bf47eb2c18ea101aad0f
jmlucjav <jmlucjav@gmail.com>
2015-05-13 11:51:02 +0200

    update/setup sh

---
+++ setupconf.sh
@@ -0,0 +1,8 @@
+# 
+cd ~
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+wget https://raw.githubusercontent.com/jmlucjav/tmuxconf/master/.tmux.conf 
+wget https://raw.githubusercontent.com/jmlucjav/tmuxconf/master/.tmux.solr.session 
+wget https://raw.githubusercontent.com/jmlucjav/tmuxconf/master/.bashrc
+wget https://raw.githubusercontent.com/jmlucjav/tmuxconf/master/.shared_aliases
+
---
+++ updateconf.sh
@@ -0,0 +1,13 @@
+# update tmp if it changed 
+cd ~/.tmux/plugins/tpm
+git pull
+
+#now my conf
+cd ~
+wget -q https://raw.githubusercontent.com/jmlucjav/tmuxconf/master/.tmux.conf -O .tmux.conf
+wget -q https://raw.githubusercontent.com/jmlucjav/tmuxconf/master/.tmux.solr.session -O .tmux.solr.conf
+wget -q https://raw.githubusercontent.com/jmlucjav/tmuxconf/master/.bashrc -O .bashrc
+wget -q https://raw.githubusercontent.com/jmlucjav/tmuxconf/master/.shared_aliases -O .shared_aliases
+
+#start session with tmux
+tmux kill-server
